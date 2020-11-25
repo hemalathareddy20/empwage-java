e78ffe7826d80bafa9eaf38815b7ce2a695cc0c5
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-25 20:29:31 +0530

    Modified:Logic to check employee part/full time wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,24 +2,25 @@
 import java.util.Random;
 public class empWageComputation {
 public static void main(String[] args) {
-   int isPresent=1;
-   int salary;
+   int empHrs;
+   int IS_PART_TIME=1;
+   int IS_FULL_TIME=2;
+   int EMP_RATE_PER_HR=20;
    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
    Random random=new Random();
-   int attendance=random.nextInt(2);
+   int attendance=random.nextInt(3);
    System.out.println("random number:"+attendance);
-   if(attendance == isPresent) {
+   if(attendance == IS_PART_TIME) 
+         empHrs=4;
 
-        System.out.println("Employee is present!");
-        int empHrs=8;
-        int ratePerHour=20;
-        salary=empHrs * ratePerHour;
-   }
-   else {
+   else if(attendance == IS_FULL_TIME)
+           empHrs=8;
 
-        System.out.println("Employee is absent!");
-        salary=0;
+   else
+        empHrs=0;
+
+  System.out.println("Employee hours: "+empHrs); 
+  int salary=empHrs * EMP_RATE_PER_HR;
+  System.out.println("Salary: "+salary);
    }
-  System.out.println("Salary: "+salary); 
-  }
 }
