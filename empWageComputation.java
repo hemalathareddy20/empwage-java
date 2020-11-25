486ce001d5d474890eb1fb6328169707f5356671
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-25 19:08:23 +0530

    Added:Random logic to check attendance

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,5 +1,14 @@
-class empWageComputation {
+
+import java.util.Random;
+public class empWageComputation {
 public static void main(String[] args) {
    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+   Random random=new Random();
+   int attendance=random.nextInt(2);
+   System.out.println("random number:"+attendance);
+   if(attendance == 1)
+   System.out.println("Employee is present!");
+   else
+   System.out.println("Employee is absent!");
   }
 }
