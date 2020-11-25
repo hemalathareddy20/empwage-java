370037265d6542970f202422840da38b97075b79
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-25 21:18:38 +0530

    Modified:Logic to check employee wage by using switch case statement

--- empWageComputation.java
+++ empWageComputation.java
@@ -3,24 +3,33 @@ import java.util.Random;
 public class empWageComputation {
 public static void main(String[] args) {
    int empHrs;
+   int salary;
    int IS_PART_TIME=1;
    int IS_FULL_TIME=2;
    int EMP_RATE_PER_HR=20;
    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
    Random random=new Random();
-   int attendance=random.nextInt(3);
-   System.out.println("random number:"+attendance);
-   if(attendance == IS_PART_TIME) 
-         empHrs=4;
-
-   else if(attendance == IS_FULL_TIME)
+   int employeeType=random.nextInt(3);
+   System.out.println("random number:"+employeeType);
+   switch (employeeType) {
+   case 1:
+           empHrs=4;
+           System.out.println("Employee hours: "+empHrs);
+           salary=(empHrs*EMP_RATE_PER_HR);
+           System.out.println("Salary: "+salary);
+           break;
+   case 2:
            empHrs=8;
-
-   else
-        empHrs=0;
-
-  System.out.println("Employee hours: "+empHrs); 
-  int salary=empHrs * EMP_RATE_PER_HR;
-  System.out.println("Salary: "+salary);
-   }
+           System.out.println("Employee hours: "+empHrs);
+           salary=(empHrs*EMP_RATE_PER_HR);
+           System.out.println("Salary: "+salary);
+           break;
+   default:
+           empHrs=0;
+           System.out.println("Employee hours: "+empHrs);
+           salary=(empHrs*EMP_RATE_PER_HR);
+           System.out.println("Salary: "+salary);
+           break;
+    }
+  }
 }
