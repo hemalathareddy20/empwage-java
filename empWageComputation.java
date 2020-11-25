d70300ca352cd899cf4f9858dff4c411369eba01
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-25 22:20:12 +0530

    Modified:Logic to check employee total salary till a condition of total working hrs or days is reached for a month

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,34 +2,35 @@ import java.util.Random;
 public class empWageComputation {
 public static void main(String[] args) {
    int empHrs;
-   int salary;
    int IS_PART_TIME=1;
    int IS_FULL_TIME=2;
    int EMP_RATE_PER_HR=20;
+   int MAX_HRS_IN_MONTH=10;
    int NUM_WORKING_DAYS=20;
-   int totalSalary=0;
+   int totalEmpHrs=0;
+   int totalWorkingDays=0;
    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-   for(int day=1;day<=NUM_WORKING_DAYS;day++ )
-   {
-    Random random=new Random();
-    int employeeType=random.nextInt(3);
-    switch (employeeType) {
-    case 1:
-           empHrs=4;
-           salary=(empHrs*EMP_RATE_PER_HR);
-           break;
-    case 2:
-           empHrs=8;
-           salary=(empHrs*EMP_RATE_PER_HR);
-           break;
-    default:
-           empHrs=0;
-           salary=(empHrs*EMP_RATE_PER_HR);
-           break;
-    }
-    System.out.println("Day: "+day+" "+"Salary: "+salary);
-    totalSalary=(totalSalary+salary);
-   }
-   System.out.println("Total Salary: "+totalSalary); 
+   while( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS ) {
+         totalWorkingDays++;
+         System.out.println("Total working day: "+totalWorkingDays);
+         Random random=new Random();
+         int employeeType=random.nextInt(3);
+         switch (employeeType) {
+          case 1:
+                 empHrs=4;
+                 break;
+          case 2:
+                 empHrs=8;
+                 break;
+         default:
+                 empHrs=0;
+                  break;
+        }
+   totalEmpHrs=(totalEmpHrs + empHrs);
+   System.out.println("Total Employee Hours: "+totalEmpHrs);
 }
+   int totalSalary=(totalEmpHrs*EMP_RATE_PER_HR);
+   System.out.println("Total Salary: "+totalSalary); 
+ 
+  }
 }
