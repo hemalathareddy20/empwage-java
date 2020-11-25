ee5e04873e307343025cc4453cb9ecd6b758c515
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-25 20:17:05 +0530

    Modified:Logic to check employee daily wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,13 +2,24 @@
 import java.util.Random;
 public class empWageComputation {
 public static void main(String[] args) {
+   int isPresent=1;
+   int salary;
    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
    Random random=new Random();
    int attendance=random.nextInt(2);
    System.out.println("random number:"+attendance);
-   if(attendance == 1)
-   System.out.println("Employee is present!");
-   else
-   System.out.println("Employee is absent!");
+   if(attendance == isPresent) {
+
+        System.out.println("Employee is present!");
+        int empHrs=8;
+        int ratePerHour=20;
+        salary=empHrs * ratePerHour;
+   }
+   else {
+
+        System.out.println("Employee is absent!");
+        salary=0;
+   }
+  System.out.println("Salary: "+salary); 
   }
 }
