9072d6188389a301f384efe46d30272bc87d035a
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-27 18:50:01 +0530

    Modified:Logic to save the Total Employee Wage for Each Company

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,12 +2,25 @@ public class empWageComputation {
    public static final int IS_PART_TIME=1;
    public static final int IS_FULL_TIME=2;
 
-   public static int computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth){
-      int empHrs=0, totalEmpHrs=0, totalWorkingDays=0;
-      while( totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays ) {
-          totalWorkingDays++;
-          int empCheck=(int)Math.floor(Math.random()*10)%3;
-          switch (empCheck) {
+        private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public empWageComputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		super();
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+       public void computeEmpWage( ) {
+       int empHrs=0, totalEmpHrs=0, totalWorkingDays=0;
+       while( totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays ) {
+         totalWorkingDays++;
+         int empCheck=(int)Math.floor(Math.random()*10)%3;
+         switch (empCheck) {
             case 1:
                  empHrs=4;
                  break;
@@ -17,16 +30,27 @@ public class empWageComputation {
             default:
                  empHrs=0;
                   break;
-        }
-        totalEmpHrs=totalEmpHrs + empHrs; 
-        System.out.println("Day: " + totalWorkingDays + "employee hours: " +empHrs);
-   }
-   int totalEmpWage=totalEmpHrs*empRatePerHour;
-   System.out.println("Total Emp Wage for Company: "+company+"is: "+totalEmpWage);
-   return totalEmpWage;
-}
-public static void main(String[] args) {
-  computeEmpWage("HCL: ", 20, 4, 10);
-  computeEmpWage("Wipro: ", 10, 6, 20);
- }
+         }
+         totalEmpHrs=totalEmpHrs + empHrs; 
+         System.out.println("Day: " + totalWorkingDays + "employee hours: " +empHrs);
+         }
+   totalEmpWage=totalEmpHrs*empRatePerHour;
+
+         }
+       public String toString() {
+		return "Total Emp Wage for Company: "+company+" is "+totalEmpWage;
+       }
+       public static void main(String[] args) {
+		empWageComputation HCL=new empWageComputation("HCL", 20, 2, 10);
+		empWageComputation Wipro=new empWageComputation("Wipro", 10, 4, 20);
+	
+		HCL.computeEmpWage();
+		System.out.println(HCL);
+		
+
+		Wipro.computeEmpWage();
+		System.out.println(Wipro);
+	
+	}
+	
 }
