cc2dc809682a97743261e2898d65040a246deb62
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-27 11:57:40 +0530

    Modified:Logic to check Employee Wage for Multiple Companies

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,35 +1,32 @@
 public class empWageComputation {
-public static final int IS_PART_TIME=1;
-public static final int IS_FULL_TIME=2;
-public static final int EMP_RATE_PER_HR=20;
-public static final int MAX_HRS_IN_MONTH=10;
-public static final int NUM_WORKING_DAYS=20;
-public static int empWageComputation(){
-int empHrs;
-int totalEmpHrs=0;
-int totalWorkingDays=0;
-while( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS ) {
-         totalWorkingDays++;
-         int empCheck=(int)Math.floor(Math.random()*10)%3;
-         switch (empCheck) {
-          case 1:
+   public static final int IS_PART_TIME=1;
+   public static final int IS_FULL_TIME=2;
+
+   public static int computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth){
+      int empHrs=0, totalEmpHrs=0, totalWorkingDays=0;
+      while( totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays ) {
+          totalWorkingDays++;
+          int empCheck=(int)Math.floor(Math.random()*10)%3;
+          switch (empCheck) {
+            case 1:
                  empHrs=4;
                  break;
-          case 2:
+            case 2:
                  empHrs=8;
                  break;
-         default:
+            default:
                  empHrs=0;
                   break;
         }
-   totalEmpHrs=(totalEmpHrs + empHrs); 
-System.out.println("Day: "+totalWorkingDays+"  "+"employee hours: "+empHrs);
+        totalEmpHrs=totalEmpHrs + empHrs; 
+        System.out.println("Day: " + totalWorkingDays + "employee hours: " +empHrs);
+   }
+   int totalEmpWage=totalEmpHrs*empRatePerHour;
+   System.out.println("Total Emp Wage for Company: "+company+"is: "+totalEmpWage);
+   return totalEmpWage;
 }
-   int totalSalary=(totalEmpHrs*EMP_RATE_PER_HR);
-   System.out.println("Total Salary: "+totalSalary);
-   return totalSalary;
-  }
-public static void main(String args[]) {
-empWageComputation();
-}
+public static void main(String[] args) {
+  computeEmpWage("HCL: ", 20, 4, 10);
+  computeEmpWage("Wipro: ", 10, 6, 20);
+ }
 }
